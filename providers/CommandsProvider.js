ffbd8cdc74c3d7cfe00ef2e9a7fd02c9470b4917
StitchNG <stitchnig@gmail.com>
2018-12-10 13:30:33 +0100

    Create CommandsProvider.js

---
+++ providers/CommandsProvider.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { ServiceProvider } = require('@adonisjs/fold')
+
+class CommandsProvider extends ServiceProvider {
+    register () {
+    
+    }
+    
+    boot(){
+    
+    }
+}
+
+module.exports = CommandsProvider
