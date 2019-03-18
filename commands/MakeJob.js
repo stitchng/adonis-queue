2b2bfe3fef536c8cc7a14148899645d6e394d7b5
StitchNG <stitchnig@gmail.com>
2019-03-18 13:52:44 +0100

    Update MakeJob.js

--- commands/MakeJob.js
+++ commands/MakeJob.js
@@ -1,1 +1,50 @@
+'use strict'
 
+const Base = require('./Job/Base')
+
+class MakeJob extends Base {
+  /**
+   * The command signature
+   *
+   * @method signature
+   *
+   * @return {String}
+   */
+  static get signature () {
+    return `
+    make:job 
+    { name: Name of the job }`
+  }
+
+  /**
+   * The command description
+   *
+   * @method description
+   *
+   * @return {String}
+   */
+  static get description () {
+    return 'Make a new job to be queued'
+  }
+
+  /**
+   * Handle method executed by ace
+   *
+   * @method handle
+   *
+   * @param  {String} name < Destructure args >
+   * @param  {Object} options
+   *
+   * @return {void}
+   */
+  async handle ({ name }, options) {
+    try {
+      await this.ensureInProjectRoot()
+      await this.generateBlueprint('job', name)
+    } catch ({ message }) {
+      this.error(message)
+    }
+  }
+}
+
+module.exports = MakeJob
