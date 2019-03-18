d3d3095dfbd7d4e4e9e5f8b58b79fddbf9b251e6
StitchNG <stitchnig@gmail.com>
2019-03-18 11:28:04 +0100

    Update QueueProvider.js

--- providers/QueueProvider.js
+++ providers/QueueProvider.js
@@ -2,14 +2,19 @@
 
 const { ServiceProvider } = require('@adonisjs/fold')
 
-class QueueProvider extends QueueProvider {
-  register () {
-    this.app.singleton('Adonis/Addons/BugSnag', () => {
-      const Config = this.app.use('Adonis/Src/Config')
-      const QueueHelper = require('../src/RedisQueue')
-      return new QueueHelper(require('bee-queue'), Config)
-    })
-  }
+class QueueProvider extends ServiceProvider {
+
+    register(){
+        this.app.singleton('Adonis/Addon/Queue', () => {
+              const Config = this.app.use('Adonis/Src/Config')
+              const Exception = this.app.use('Exception')
+              const QueueHelper = require('../src/RedisQueue')
+              
+              return new QueueHelper(require('bee-queue'), Exception, Config)
+        })
+      
+        this.app.alias('Adonis/Addon/Queue', 'Queue')
+    }
 }
 
 module.exports = QueueProvider
